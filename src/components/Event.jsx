2afe3d6612c4b0600dd3e4ff85fa7f0bdb877f89
Andrew Wand <andrewwands@gmail.com>
2022-10-29 19:47:10 -0500

    added anim for event section

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/event.css";
 import eventImg from "../imgs/image-2.png";
 
 function Event() {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 1400);
+      setScroll(true);
+    });
+  }, []);
+
   return (
     <section className="event-section">
       <div className="container">
         <div className="inner-event">
-          <figure className="event-img">
+          <figure
+            className={
+              scroll ? "event-img animated fadeInLeft" : "event-img beforeAnim"
+            }
+          >
             <img src={eventImg} alt="" />
           </figure>
-          <div className="event-content">
+          <div
+            className={
+              scroll
+                ? "event-content animated fadeInRight"
+                : "event-content beforeAnim"
+            }
+          >
             <h3 className="event-title">
               YOU'LL LOVE OUR BREAKFAST TACOS, BOWLS AND PLATES!
             </h3>
